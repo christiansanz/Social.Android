a46877c9f5280d35f8ee3205f4e4a3eabd97aadf
Christian Sanz <espartano_alex@hotmail.com>
2014-12-18 18:44:12 +0100

    pruebas

--- segundaprueba/src/android/support/v7/app/ActionBarActivity.java
+++ segundaprueba/src/android/support/v7/app/ActionBarActivity.java
@@ -1,5 +1,24 @@
 package android.support.v7.app;
 
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 public class ActionBarActivity {
+
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
